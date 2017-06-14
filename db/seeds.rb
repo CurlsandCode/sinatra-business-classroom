b303c925e7e191ae7e8b7a76b6ccf4503dcf9e67
WilliamBarela <william.barela.dev@gmail.com>
2017-06-15 02:01:03 +0900

    seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 # Creates student instances
-Student.create({:username=>"username1", :first_name=>"Liping", :last_name=>"Xiao", :nickname=>"Li", :major=>"Accounting", :photo=>"http://imgur.com/jgbL2Sx.jpg", :email=>"email@domain.com", :password=>"password"})
-Student.create({:username=>"username2", :first_name=>"Sebastian", :last_name=>"Bach", :nickname=>"Basti", :major=>"Economics", :photo=>"http://imgur.com/2KFsJfe.jpg", :email=>"email@domain.com", :password=>"password"})
-Student.create({:username=>"username3", :first_name=>"Sara", :last_name=>"Carothers", :nickname=>"Beebee", :major=>"Finance", :photo=>"http://i.imgur.com/FXJiQcy.jpg", :email=>"email@domain.com", :password=>"password"})
-Student.create({:username=>"username4", :first_name=>"Frederick", :last_name=>"Chopin", :nickname=>"Freddy", :major=>"Management", :photo=>"http://i.imgur.com/x1t2grP.jpg", :email=>"email@domain.com", :password=>"password"})
-Student.create({:username=>"username5", :first_name=>"Antonin", :last_name=>"Dvorac", :nickname=>"Toni", :major=>"Computer Management Information Systems", :photo=>"http://i.imgur.com/NUR4HFK.png", :email=>"email@domain.com", :password=>"password"})
-Student.create({:username=>"username6", :first_name=>"Henry", :last_name=>"Purcell", :nickname=>"Ri", :major=>"International Business", :photo=>"http://imgur.com/RZt07dq.jpg", :email=>"email@domain.com", :password=>"password"})
-Student.create({:username=>"username7", :first_name=>"Claude", :last_name=>"Debussy", :nickname=>"Clau", :major=>"General Business", :photo=>"http://i.imgur.com/z87cszh.jpg", :email=>"email@domain.com", :password=>"password"})
-Student.create({:username=>"username8", :first_name=>"Charles", :last_name=>"Dickens", :nickname=>"Charlie", :major=>"Marketing", :photo=>"http://i.imgur.com/9ZCUerO.jpg", :email=>"email@domain.com", :password=>"password"})
+Student.create({:username=>"username1", :first_name=>"Liping", :last_name=>"Xiao", :nickname=>"Li", :major=>"Accounting", :photo_link=>"http://imgur.com/jgbL2Sx.jpg", :email=>"email@domain.com", :password=>"password"})
+Student.create({:username=>"username2", :first_name=>"Sebastian", :last_name=>"Bach", :nickname=>"Basti", :major=>"Economics", :photo_link=>"http://imgur.com/2KFsJfe.jpg", :email=>"email@domain.com", :password=>"password"})
+Student.create({:username=>"username3", :first_name=>"Sara", :last_name=>"Carothers", :nickname=>"Beebee", :major=>"Finance", :photo_link=>"http://i.imgur.com/FXJiQcy.jpg", :email=>"email@domain.com", :password=>"password"})
+Student.create({:username=>"username4", :first_name=>"Frederick", :last_name=>"Chopin", :nickname=>"Freddy", :major=>"Management", :photo_link=>"http://i.imgur.com/x1t2grP.jpg", :email=>"email@domain.com", :password=>"password"})
+Student.create({:username=>"username5", :first_name=>"Antonin", :last_name=>"Dvorac", :nickname=>"Toni", :major=>"Computer Management Information Systems", :photo_link=>"http://i.imgur.com/NUR4HFK.png", :email=>"email@domain.com", :password=>"password"})
+Student.create({:username=>"username6", :first_name=>"Henry", :last_name=>"Purcell", :nickname=>"Ri", :major=>"International Business", :photo_link=>"http://imgur.com/RZt07dq.jpg", :email=>"email@domain.com", :password=>"password"})
+Student.create({:username=>"username7", :first_name=>"Claude", :last_name=>"Debussy", :nickname=>"Clau", :major=>"General Business", :photo_link=>"http://i.imgur.com/z87cszh.jpg", :email=>"email@domain.com", :password=>"password"})
+Student.create({:username=>"username8", :first_name=>"Charles", :last_name=>"Dickens", :nickname=>"Charlie", :major=>"Marketing", :photo_link=>"http://i.imgur.com/9ZCUerO.jpg", :email=>"email@domain.com", :password=>"password"})
 
 # Creates presentation instances
 Presentation.create({:presentation_type => "assignment", :youtube_link => "https://youtu.be/gFo6pX-oJ9w", :student_id => 1, :deadline => "2017-07-07"})
