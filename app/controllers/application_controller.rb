3b0c718079bc67fa23063c1dfbd87777718b002d
WilliamBarela <william.barela.dev@gmail.com>
2017-06-19 00:59:50 +0900

    added additional views for assignments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,4 +20,15 @@ class ApplicationController < Sinatra::Base
   get "/signup" do
     erb :signup
   end
+
+  helpers do
+    def current_user
+      User.find(session[:user_id])
+    end
+
+    def logged_in?
+      !!session[:user_id]
+    end
+  end
+
 end
--- app/controllers/presentations_controller.rb
+++ app/controllers/presentations_controller.rb
@@ -5,4 +5,20 @@ class PresentationsController < ApplicationController
     erb :"presentations/index"
   end
 
+  # to make this work properly, include current_user method to check for validity
+  get "/presentations/assignment/new" do
+    erb :"/presentations/assignment_new"
+  end
+
+  get "/presentations/introduction/new" do
+    erb :"/presentations/introduction_new"
+  end
+  
+  get "/presentations/assignment/edit" do
+    erb :"/presentations/assignment_edit"
+  end
+
+  get "/presentations/introduction/edit" do
+    erb :"/presentations/introduction_edit"
+  end
 end
