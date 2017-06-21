c8ceadcf19ccc7ed1354ac67e60ff8875e82c175
WilliamBarela <william.barela.dev@gmail.com>
2017-06-21 15:52:00 +0900

    corrected seeds.rb to adjust for uniqueness of email and username

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 # Creates student instances
-Student.create({:username=>"username1", :first_name=>"Liping", :last_name=>"Xiao", :nickname=>"Li", :major=>"Accounting", :photo_link=>"http://imgur.com/jgbL2Sx.jpg", :email=>"email@domain.com", :password=>"password"})
-Student.create({:username=>"username2", :first_name=>"Sebastian", :last_name=>"Bach", :nickname=>"Basti", :major=>"Economics", :photo_link=>"http://imgur.com/2KFsJfe.jpg", :email=>"email@domain.com", :password=>"password"})
-Student.create({:username=>"username3", :first_name=>"Sara", :last_name=>"Carothers", :nickname=>"Beebee", :major=>"Finance", :photo_link=>"http://i.imgur.com/FXJiQcy.jpg", :email=>"email@domain.com", :password=>"password"})
-Student.create({:username=>"username4", :first_name=>"Frederick", :last_name=>"Chopin", :nickname=>"Freddy", :major=>"Management", :photo_link=>"http://i.imgur.com/x1t2grP.jpg", :email=>"email@domain.com", :password=>"password"})
-Student.create({:username=>"username5", :first_name=>"Antonin", :last_name=>"Dvorac", :nickname=>"Toni", :major=>"Computer Management Information Systems", :photo_link=>"http://i.imgur.com/NUR4HFK.png", :email=>"email@domain.com", :password=>"password"})
-Student.create({:username=>"username6", :first_name=>"Henry", :last_name=>"Purcell", :nickname=>"Ri", :major=>"International Business", :photo_link=>"http://imgur.com/RZt07dq.jpg", :email=>"email@domain.com", :password=>"password"})
-Student.create({:username=>"username7", :first_name=>"Claude", :last_name=>"Debussy", :nickname=>"Clau", :major=>"General Business", :photo_link=>"http://i.imgur.com/z87cszh.jpg", :email=>"email@domain.com", :password=>"password"})
-Student.create({:username=>"username8", :first_name=>"Charles", :last_name=>"Dickens", :nickname=>"Charlie", :major=>"Marketing", :photo_link=>"http://i.imgur.com/9ZCUerO.jpg", :email=>"email@domain.com", :password=>"password"})
+Student.create({:username=>"username1", :first_name=>"Liping", :last_name=>"Xiao", :nickname=>"Li", :major=>"Accounting", :photo_link=>"http://imgur.com/jgbL2Sx.jpg", :email=>"email1@domain.com", :password=>"password"})
+Student.create({:username=>"username2", :first_name=>"Sebastian", :last_name=>"Bach", :nickname=>"Basti", :major=>"Economics", :photo_link=>"http://imgur.com/2KFsJfe.jpg", :email=>"email2@domain.com", :password=>"password"})
+Student.create({:username=>"username3", :first_name=>"Sara", :last_name=>"Carothers", :nickname=>"Beebee", :major=>"Finance", :photo_link=>"http://i.imgur.com/FXJiQcy.jpg", :email=>"email3@domain.com", :password=>"password"})
+Student.create({:username=>"username4", :first_name=>"Frederick", :last_name=>"Chopin", :nickname=>"Freddy", :major=>"Management", :photo_link=>"http://i.imgur.com/x1t2grP.jpg", :email=>"email4@domain.com", :password=>"password"})
+Student.create({:username=>"username5", :first_name=>"Antonin", :last_name=>"Dvorac", :nickname=>"Toni", :major=>"Computer Management Information Systems", :photo_link=>"http://i.imgur.com/NUR4HFK.png", :email=>"email5@domain.com", :password=>"password"})
+Student.create({:username=>"username6", :first_name=>"Henry", :last_name=>"Purcell", :nickname=>"Ri", :major=>"International Business", :photo_link=>"http://imgur.com/RZt07dq.jpg", :email=>"email6@domain.com", :password=>"password"})
+Student.create({:username=>"username7", :first_name=>"Claude", :last_name=>"Debussy", :nickname=>"Clau", :major=>"General Business", :photo_link=>"http://i.imgur.com/z87cszh.jpg", :email=>"email7@domain.com", :password=>"password"})
+Student.create({:username=>"username8", :first_name=>"Charles", :last_name=>"Dickens", :nickname=>"Charlie", :major=>"Marketing", :photo_link=>"http://i.imgur.com/9ZCUerO.jpg", :email=>"email8@domain.com", :password=>"password"})
 
 # Creates presentation instances
 Presentation.create({:presentation_type => "assignment", :youtube_link => "https://youtu.be/gFo6pX-oJ9w", :student_id => 1, :deadline => "2017-07-07"})
@@ -17,4 +17,14 @@ Presentation.create({:presentation_type => "assignment", :youtube_link => "https
 Presentation.create({:presentation_type => "assignment", :youtube_link => "https://youtu.be/LTCVDI05ccE", :student_id => 6, :deadline => "2017-07-12"})
 Presentation.create({:presentation_type => "assignment", :youtube_link => "https://youtu.be/CO6RhAMLSWU", :student_id => 7, :deadline => "2017-07-13"})
 Presentation.create({:presentation_type => "assignment", :youtube_link => "https://youtu.be/F0GLxF4gXKA", :student_id => 8, :deadline => "2017-07-14"})
+
+# Creates presentation instances
+Presentation.create({:presentation_type => "introduction", :youtube_link => "https://youtu.be/B-nElBVAoPg", :student_id => 1, :deadline => "2017-07-07"})
+Presentation.create({:presentation_type => "introduction", :youtube_link => "https://youtu.be/blBRFaia07s", :student_id => 2, :deadline => "2017-07-08"})
+Presentation.create({:presentation_type => "introduction", :youtube_link => "https://youtu.be/QvaazeOjVBY", :student_id => 3, :deadline => "2017-07-09"})
+Presentation.create({:presentation_type => "introduction", :youtube_link => "https://youtu.be/6PeW_G0PFb4", :student_id => 4, :deadline => "2017-07-10"})
+Presentation.create({:presentation_type => "introduction", :youtube_link => "https://youtu.be/M61go5REUSk", :student_id => 5, :deadline => "2017-07-11"})
+Presentation.create({:presentation_type => "introduction", :youtube_link => "https://youtu.be/39iDLQVXq0U", :student_id => 6, :deadline => "2017-07-12"})
+Presentation.create({:presentation_type => "introduction", :youtube_link => "https://youtu.be/BBrA-E1Mk8c", :student_id => 7, :deadline => "2017-07-13"})
+Presentation.create({:presentation_type => "introduction", :youtube_link => "https://youtu.be/lKTXI4m6MdI", :student_id => 8, :deadline => "2017-07-14"})
 
